fba1bab295834c17d21de54dbc5da79e510a2ce1
PIT <pi4git@list.ru>
2022-04-10 22:04:11 +0500

    домашка ООП

--- src/main/java/hw09/CarMoving.java
+++ src/main/java/hw09/CarMoving.java
@@ -4,5 +4,6 @@ public class CarMoving {
 
     public static void main(String[] args) {
         System.out.printf("Проверка проекта qwe \n");
+        System.out.println("Проверка проекта кен");
     }
 }
