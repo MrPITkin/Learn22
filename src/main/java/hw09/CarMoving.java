edb193f53ded97e38a3d4c869f6c60c2db70fba7
PIT <pi4git@list.ru>
2022-04-10 23:39:59 +0500

    домашка ООП

--- src/main/java/hw09/CarMoving.java
+++ src/main/java/hw09/CarMoving.java
@@ -3,9 +3,9 @@ package hw09;
 public class CarMoving {
 
     public static void main(String[] args) {
-        System.out.printf("Проверка проекта qwe \n");
-        System.out.println("Проверка проекта кен");
-        System.out.println("катироФФФФка");
-        System.out.println("Russian по русски");
+        char testo = 'РЃ';
+        System.out.printf("write СЂСѓСЃСЃРєСѓСЋ Р±СѓРєРѕРІСѓ \"%s\".\n", testo);
+        System.out.println("СЂР°С€Р° ? ofcouse ");
+
     }
 }
