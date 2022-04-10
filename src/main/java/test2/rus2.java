667805a18dd5959b46b37fcc6db7d4c504e56e6a
PIT <pi4git@list.ru>
2022-04-10 23:28:00 +0500

    домашка ООП

--- src/main/java/test2/rus2.java
+++ src/main/java/test2/rus2.java
@@ -3,5 +3,8 @@ package test2;
 public class rus2 {
     public static void main(String[] args) {
         System.out.println("и снова ofcouse ");
+        System.out.println("и снова ofcouse ");
+        char testo = 'Ё';
+        System.out.printf("бреем русскую букову \"%s\".\n", testo);
     }
 }
