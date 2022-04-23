46723303f9839b135cbffce7d7f6f1f49f981f40
PIT <pi4git@list.ru>
2022-04-23 22:23:17 +0500

    заметки, литералы

---
+++ src/main/java/note/Literals.java
@@ -0,0 +1,8 @@
+package note;
+
+public class Literals {
+    public static void main(String[] args) {
+        System.out.println(0x12.4P-1); // шестнадцатиричный литерал 18.25 * (2 в степени -1)
+
+    }
+}
