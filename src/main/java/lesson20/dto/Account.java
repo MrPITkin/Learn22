723b2a2c724ec0967a03ce55f7c1e02bfc05b86a
PIT <pi4git@list.ru>
2022-07-08 18:19:35 +0500

    XML маршалинг и анмаршалинг

---
+++ src/main/java/lesson20/dto/Account.java
@@ -0,0 +1,18 @@
+package lesson20.dto;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@XmlRootElement
+public class Account {
+//    @JsonProperty("number")
+//    @CsvBindByName(column = "Номер счета")
+    private String accountNumber;
+//    @CsvBindByName(column = "Баланс")
+    private BigDecimal balance;
+//    @JsonIgnore
+    private Bank bank;
+}
